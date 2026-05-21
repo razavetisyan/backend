c89522d78d3615622172276bbd92efe13dfefb54
razavetisyan <razavetisyan123@gmail.com>
2026-05-21 19:55:52 +0400

    personal-hub

--- personal-hub/src/routes/habits.routes.js
+++ personal-hub/src/routes/habits.routes.js
@@ -7,7 +7,7 @@ const HabitsController = require("../controllers/habits.controller.js");
 router.get("/", authMiddleware, HabitsController.getHabitByOwnerId);
 router.get("/:id", authMiddleware, HabitsController.getHabitById);
 router.post("/", authMiddleware, validateHabit, HabitsController.createHabit);
-router.post("/:id/check-in", authMiddleware, validateHabit, HabitsController.createHabit);
+router.post("/:id/check-in", authMiddleware, validateHabit, HabitsController.checkInHabit);
 router.patch("/:id", authMiddleware, validateHabit, HabitsController.updateHabit);
 router.delete("/:id", authMiddleware, HabitsController.deleteHabit);
 
